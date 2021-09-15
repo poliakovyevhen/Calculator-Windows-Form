756adf093b7cc9387b61d4d1375a123cc705a920
Yevhen Poliakov <90283399+poliakovyevhen@users.noreply.github.com>
2021-09-15 21:08:17 +0300

    Calculator-Windows-Form
    Calculator-Windows-Form

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Калькулятор
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_result = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label_symbol = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.radioButton_add = new System.Windows.Forms.RadioButton();
+            this.radioButton_sub = new System.Windows.Forms.RadioButton();
+            this.radioButton_mult = new System.Windows.Forms.RadioButton();
+            this.radioButton_div = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_result
+            // 
+            this.button_result.Location = new System.Drawing.Point(78, 360);
+            this.button_result.Margin = new System.Windows.Forms.Padding(4);
+            this.button_result.Name = "button_result";
+            this.button_result.Size = new System.Drawing.Size(112, 34);
+            this.button_result.TabIndex = 0;
+            this.button_result.Text = "button1";
+            this.button_result.UseVisualStyleBackColor = true;
+            this.button_result.Click += new System.EventHandler(this.button_result_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(287, 396);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(233, 23);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "округляти до знаків після коми";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "HEX",
+            "DEC",
+            "OCT",
+            "BIN",
+            "???"});
+            this.comboBox1.Location = new System.Drawing.Point(324, 278);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 27);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label_symbol
+            // 
+            this.label_symbol.AutoSize = true;
+            this.label_symbol.Location = new System.Drawing.Point(124, 50);
+            this.label_symbol.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_symbol.Name = "label_symbol";
+            this.label_symbol.Size = new System.Drawing.Size(16, 19);
+            this.label_symbol.TabIndex = 3;
+            this.label_symbol.Text = "?";
+            this.label_symbol.Click += new System.EventHandler(this.label_symbol_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(384, 121);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(78, 19);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "копіювати";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(404, 362);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(180, 26);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(78, 278);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar1.Maximum = 4;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(150, 34);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // radioButton_add
+            // 
+            this.radioButton_add.AutoSize = true;
+            this.radioButton_add.Location = new System.Drawing.Point(78, 172);
+            this.radioButton_add.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton_add.Name = "radioButton_add";
+            this.radioButton_add.Size = new System.Drawing.Size(36, 23);
+            this.radioButton_add.TabIndex = 7;
+            this.radioButton_add.TabStop = true;
+            this.radioButton_add.Text = "+";
+            this.radioButton_add.UseVisualStyleBackColor = true;
+            this.radioButton_add.CheckedChanged += new System.EventHandler(this.radioButton_add_CheckedChanged);
+            // 
+            // radioButton_sub
+            // 
+            this.radioButton_sub.AutoSize = true;
+            this.radioButton_sub.Location = new System.Drawing.Point(139, 172);
+            this.radioButton_sub.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton_sub.Name = "radioButton_sub";
+            this.radioButton_sub.Size = new System.Drawing.Size(33, 23);
+            this.radioButton_sub.TabIndex = 8;
+            this.radioButton_sub.TabStop = true;
+            this.radioButton_sub.Text = "-";
+            this.radioButton_sub.UseVisualStyleBackColor = true;
+            this.radioButton_sub.CheckedChanged += new System.EventHandler(this.radioButton_sub_CheckedChanged);
+            // 
+            // radioButton_mult
+            // 
+            this.radioButton_mult.AutoSize = true;
+            this.radioButton_mult.Location = new System.Drawing.Point(78, 206);
+            this.radioButton_mult.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton_mult.Name = "radioButton_mult";
+            this.radioButton_mult.Size = new System.Drawing.Size(35, 23);
+            this.radioButton_mult.TabIndex = 9;
+            this.radioButton_mult.TabStop = true;
+            this.radioButton_mult.Text = "*";
+            this.radioButton_mult.UseVisualStyleBackColor = true;
+            this.radioButton_mult.CheckedChanged += new System.EventHandler(this.radioButton_mult_CheckedChanged);
+            // 
+            // radioButton_div
+            // 
+            this.radioButton_div.AutoSize = true;
+            this.radioButton_div.Location = new System.Drawing.Point(139, 206);
+            this.radioButton_div.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton_div.Name = "radioButton_div";
+            this.radioButton_div.Size = new System.Drawing.Size(31, 23);
+            this.radioButton_div.TabIndex = 10;
+            this.radioButton_div.TabStop = true;
+            this.radioButton_div.Text = "/";
+            this.radioButton_div.UseVisualStyleBackColor = true;
+            this.radioButton_div.CheckedChanged += new System.EventHandler(this.radioButton_div_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 47);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(90, 26);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "9";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(148, 47);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(91, 26);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.Text = "9";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Location = new System.Drawing.Point(273, 47);
+            this.textBox_result.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.ReadOnly = true;
+            this.textBox_result.Size = new System.Drawing.Size(148, 26);
+            this.textBox_result.TabIndex = 13;
+            this.textBox_result.Text = "???";
+            this.textBox_result.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 50);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 19);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "=";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 19);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Система числення";
+            // 
+            // Калькулятор
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 447);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton_div);
+            this.Controls.Add(this.radioButton_mult);
+            this.Controls.Add(this.radioButton_sub);
+            this.Controls.Add(this.radioButton_add);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label_symbol);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button_result);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Location = new System.Drawing.Point(13, 3);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Калькулятор";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Калькулятор_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_result;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label_symbol;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RadioButton radioButton_add;
+        private System.Windows.Forms.RadioButton radioButton_sub;
+        private System.Windows.Forms.RadioButton radioButton_mult;
+        private System.Windows.Forms.RadioButton radioButton_div;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Калькулятор : Form
+    {
+        public Калькулятор()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "DEC") checkBox1.Enabled = true;
+            else checkBox1.Enabled = false;
+            checkBox1.Checked = false;
+            numericUpDown1.Value = 3;
+            стираниеРезультата();
+        }
+
+        private void label_symbol_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Калькулятор_Load(object sender, EventArgs e)
+        {
+            radioButton_add.Checked = true;
+            comboBox1.SelectedItem = "DEC";
+        }
+
+        private void radioButton_add_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_add.Checked == true) label_symbol.Text = "+";
+            progressBar1.Value = 1;
+            стираниеРезультата();
+        }
+
+        private void radioButton_sub_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_sub.Checked == true) label_symbol.Text = "-";
+            progressBar1.Value = 2;
+            стираниеРезультата();
+        }
+
+        private void radioButton_mult_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_mult.Checked == true) label_symbol.Text = "*";
+            progressBar1.Value = 3;
+            стираниеРезультата();
+        }
+
+        private void radioButton_div_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_div.Checked == true) label_symbol.Text = "/";
+            progressBar1.Value = 4;
+            стираниеРезультата();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true) numericUpDown1.Enabled = true;
+            else numericUpDown1.Enabled = false;
+        }
+
+        private void button_result_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "") textBox1.Text = "1";
+            if (textBox2.Text == "") textBox2.Text = "1";
+
+            char[] c = textBox1.Text.ToCharArray();
+            int a = c.Length;
+            int d = 0;
+            for (int i = 0; i < a; i++)
+                if (c[i] == ',') d = d + 1;
+            if (d == 2) MessageBox.Show("Забагато ком");
+
+            double x1 = Convert.ToDouble(textBox1.Text);
+            double x2 = Double.Parse(textBox2.Text);
+            string symbol = label_symbol.Text;
+            double y;
+            switch (symbol)
+            {
+                case "+":
+                    y = x1 + x2;
+                    break;
+                case "-":
+                    y = x1 - x2;
+                    break;
+                case "*":
+                    y = x1 * x2;
+                    break;
+                case "/":
+                    y = x1 / x2;
+                    break;
+                case "^":
+                    y = Math.Pow(x1, x2);
+                    break;
+
+                default:
+                    y = 0;
+                    break;
+            }
+
+            int precision = (int)numericUpDown1.Value;
+            y = Math.Round(y, precision);
+
+            symbol = Convert.ToString(comboBox1.SelectedItem);
+            string result;
+            switch (symbol)
+            {
+                case "HEX":
+                    result = Convert.ToString((int)y, 16);
+                    result = result.ToUpper();
+                    break;
+                case "DEC":
+                    result = Convert.ToString(y);
+                    break;
+                case "OCT":
+                    result = Convert.ToString((int)y, 8);
+                    break;
+                case "BIN":
+                    result = Convert.ToString((int)y, 2);
+                    break;
+                default:
+                    MessageBox.Show("Така система числення не запрограмована", "Увага", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    textBox_result.Text = "???";
+                    return;
+                    //break;
+            }
+
+            textBox_result.Text = result;
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            //TextBox textBox = (TextBox)sender;
+            //textBox.SelectAll();
+            (sender as TextBox).SelectAll();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char[] c = textBox1.Text.ToCharArray();
+            int a = c.Length;
+            int d = 0;
+
+            //if (c[0] == ',')
+            //textBox1.Text = "0,";
+
+            //for (int i = 0; i < a; i++)
+            //if (c[i] == ',') d = d + 1;
+
+            //if (d == 1)
+            //{
+            //if (e.KeyChar == ',')
+            //{
+            //e.Handled = true;
+            //}
+            //}
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void стираниеРезультата()
+        {
+            textBox_result.Text = "???";
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char[] c2 = textBox2.Text.ToCharArray();
+            int a2 = c2.Length;
+            int d2 = 0;
+
+            if (c2[0] == ',')
+                textBox2.Text = "0,";
+
+            for (int i = 0; i < a2; i++)
+                if (c2[i] == ',') d2 = d2 + 1;
+
+            if (d2 == 1)
+            {
+                if (e.KeyChar == ',')
+                {
+                    e.Handled = true;
+                }
+            }
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Clipboard.Clear();    //Clear if any old value is there in Clipboard        
+            Clipboard.SetText(textBox_result.Text); //Copy text to Clipboard
+            string strClip = Clipboard.GetText(); //Get text from Clipboard
+
+            MessageBox.Show("Результат \n" + strClip + "\n скопійовано \n\n тепер можете нажати Ctrl+V \n і вставити текст кудись", "Копія у буфер обміну", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+    }
+}
